b65e3bda451e94e85d45495a6f236fad591cbbe8
jeffrey Miog <jeffrey.miog@gmail.com>
2016-10-17 19:29:00 +0200

    now checks out all files in folder

--- server_script/iot.py
+++ server_script/iot.py
@@ -5,6 +5,9 @@ import PIL.Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from geopy.geocoders import Nominatim
 
+upload_dir_path = "/home/jeffrey/Dropbox/Camera Uploads/"
+upload_dir_path_no_space = "/home/jeffrey/Dropbox/Camera\ Uploads/"
+
 
 # https://gist.github.com/erans/983821
 def get_exif_data(image):
@@ -33,6 +36,7 @@ def _get_if_exist(data, key):
 
     return None
 
+
 # https://gist.github.com/erans/983821
 def _convert_to_degress(value):
     """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
@@ -49,6 +53,7 @@ def _convert_to_degress(value):
     s = float(s0) / float(s1)
 
     return d + (m / 60.0) + (s / 3600.0)
+
 
 # https://gist.github.com/erans/983821
 def get_lat_lon(exif_data):
@@ -76,6 +81,12 @@ def get_lat_lon(exif_data):
     return lat, lon
 
 
+def get_time_pic_taken(exif_data):
+    if "DateTimeOriginal" in exif_data:
+        date = exif_data["DateTimeOriginal"]
+        return date
+
+# stack overflow
 def run_script(script, stdin=None):
     """Returns (stdout, stderr), raises error on non-zero return code"""
     import subprocess
@@ -90,6 +101,7 @@ def run_script(script, stdin=None):
         raise ScriptException(proc.returncode, stdout, stderr, script)
     return stdout, stderr
 
+
 class ScriptException(Exception):
     def __init__(self, returncode, stdout, stderr, script):
         self.returncode = returncode
@@ -103,6 +115,7 @@ def remove_spaces():
     for picture_name in pic_list:
         os.rename(os.path.join(upload_dir_path, picture_name), os.path.join(upload_dir_path, picture_name.replace(' ', '-')))
 
+
 def get_lp_and_confidence(data):
     # data = "plate0: 10 results \n\t- H786P0J\tconfidence: 89.8356\n\t- H786POJ\t confidence: 87.6114\n"
     line = data.split("\n")
@@ -114,35 +127,58 @@ def get_lp_and_confidence(data):
     print(conf_float)
     return lp_string, conf_float
 
-upload_dir_path = "/home/jeffrey/Dropbox/Camera Uploads/"
-img = PIL.Image.open(upload_dir_path + "2016-10-17 15.04.14.jpg")
-exif_data = get_exif_data(img)
-lat, lon = get_lat_lon(exif_data)
-print(get_lat_lon(exif_data))
-location_string = (str(lat) + ', ' + str(lon))
-geolocator = Nominatim()
-print(location_string)
-location = geolocator.reverse(location_string)
-print(location.address)
-# upload_dir_path_no_space = "/home/jeffrey/Dropbox/Camera\ Uploads/"
-# print("IOT-license plate recognition example")
-# new_pictures = os.listdir(upload_dir_path)
-#
-# # bash and python handle spaces differently,
-# # remove all spaces from picture names
-# remove_spaces()
-# for pic_name in new_pictures:
-#     full_command = "alpr -c eu " + upload_dir_path_no_space + pic_name
-#     output, err = run_script(full_command)
-#     output_string = output.decode(encoding='utf-8')
-#     if "results" in output_string:
-#         lp, conf = get_lp_and_confidence(output_string)
-#
-#         # strip location + time + date
-#         #store lp + confidence
-#
-#         # check if matches data from db
-#         # notify user
-#         # put data in .csv
 
-#time.sleep(5.0)
+def get_address(lat, lon):
+    if (lat != None and lon != None):
+        location_string = (str(lat) + ', ' + str(lon))
+        print(location_string)
+        geolocator = Nominatim()
+        location = geolocator.reverse(location_string)
+        return location.address
+    else:
+        return "not available"
+
+
+def get_exif(pic_name):
+    img = PIL.Image.open(upload_dir_path + pic_name)
+    exif_data = get_exif_data(img)
+    return exif_data
+
+
+def get_address_by_gps(exif_data):
+    lat, lon = get_lat_lon(exif_data)
+    address = get_address(lat, lon)
+    return address
+
+
+print("IOT-license plate recognition example")
+new_pictures = os.listdir(upload_dir_path)
+# bash and python handle spaces differently,
+# remove all spaces from picture names
+remove_spaces()
+new_pictures = os.listdir(upload_dir_path)
+print("IOT-license plate recognition example")
+for pic_name in new_pictures:
+    print(pic_name)
+    full_command = "alpr -c eu " + upload_dir_path_no_space + pic_name
+    output, err = run_script(full_command)
+    output_string = output.decode(encoding='utf-8')
+    print (output_string)
+    if "results" in output_string:
+        # strip location + time + date
+        lp, conf = get_lp_and_confidence(output_string)
+
+        exif = get_exif(pic_name)
+        address = get_address_by_gps(exif)
+        time = get_time_pic_taken(exif)
+        print("addres is:")
+        print(address)
+        print("time pic taken is:")
+        print(time)
+
+        # store lp + confidence
+        # check if matches data from db
+        # notify user
+        # put data in .csv
+    else:
+        print(output_string)
