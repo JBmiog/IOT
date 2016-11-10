71ad19fb975ea5c22b88ee154b064275f9c6efaa
jeffrey Miog <jeffrey.miog@gmail.com>
2016-11-10 15:10:42 +0100

    added tx email test case

--- server_script/email_handler.py
+++ server_script/email_handler.py
@@ -1,6 +1,23 @@
-email_config.server.ehlo()
-def tx_email(message_data):
-    email_config.server.starttls()
-    email_config.server.login(email_config.username, email_config.password)
-    email_config.server.sendmail(email_config.fromaddr, email_config.toaddrs, message_data)
-    email_config.server.quit()
+import smtplib
+import email_config
+
+class Emailer():
+    server = None
+
+    def __init__(self):
+        try:
+            self.server = smtplib.SMTP('smtp.gmail.com:587')
+        except ConnectionError as err:
+            print("cannot connect mail-server", err)
+
+    def tx_email(self, message_data):
+        try:
+            self.server.ehlo()
+            self.server.starttls()
+            self.server.login(email_config.username, email_config.password)
+            self.server.sendmail(email_config.fromaddr, email_config.toaddrs, message_data)
+            self.server.quit()
+            return 1
+        except ConnectionError as err:
+            print("cannot tx email; ", err)
+            return 0
---
+++ server_script/tests/email_handler_tests.py
@@ -0,0 +1,11 @@
+import unittest
+import email_handler
+
+
+class EmailHandlerTest(unittest.TestCase):
+
+    def test_tx_email(self):
+        message = "Subject: License plates scan results\n\n lalala"
+        ms = email_handler.Emailer()
+        result = ms.tx_email(message)
+        self.assertTrue(result)
